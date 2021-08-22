a0ac7d36875f83c13623a45cf1afbb4ab6717723
Joshua Ogwang <joshua.ogwang@hotmail.com>
2021-08-23 00:14:13 +0300

    Initial commit + Mobile first CSS

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WP Dodge</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <!-- navigation -->
+        <nav>
+            <div id="logo">
+                <a href="#">WP Dodge</a>
+            </div>
+            <div id="links">
+                    <a href="#">About Us</a>
+                    <a href="#">Product & Services</a>
+                    <a href="#">Contact Us</a>
+            </div>
+        </nav>
+        <!-- hero banner -->
+        <div class="container" id="hero_banner">
+            <div id="hero_text">
+                <h1>WP Dodge</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.<br> elit Repellat, labore vitae.</p>
+            </div>
+            <div id="hero_image">
+            </div>
+        </div>
+    </header>
+
+    <section id="benefit_content">
+    <div class="container">
+        <!-- benefit content -->
+        <h2 id="header">Lorem ipsum dolor sit amet consectetur adipisicing.</h2>
+        <p id="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit quis rerum quod dolore porro officia dolorem.</p>
+        <div class="flex-wrapper">
+            <div class="box">
+                <div class="header_image" style="background-image: url('./assets/images/tires.jpg');"></div>
+                <div class="card-text">
+                <h3>WP Dodge Garage</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta repellendus nulla reiciendis ipsum excepturi vitae rem obcaecati commodi fugit ullam!</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="header_image" style="background-image: url('./assets/images/engine.jpg');"></div>
+                <div class="card-text">
+                <h3>WP Dodge Garage</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta repellendus nulla reiciendis ipsum excepturi vitae rem obcaecati commodi fugit ullam!</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="header_image" style="background-image: url('./assets/images/car-wash.jpg')"></div>
+                <div class="card-text">
+                <h3>WP Dodge Garage</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta repellendus nulla reiciendis ipsum excepturi vitae rem obcaecati commodi fugit ullam!</p>                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+        <!-- General content -->
+        <section id="general_content">
+            <div class="container">
+                <div class="content">
+                    <div class="text">
+                        <h2>Lorem ipsum dolor sit amet.</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eius quidem qui sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. A, cupiditate!</p>
+                    </div>
+    
+                    <div class="image" style="background-image: url('./assets/images/content-1.jpg');">
+                    </div>
+                </div>
+
+                <div class="content">
+                    <div class="image" style="background-image: url('./assets/images/content-2.jpg');">
+                    </div>
+                    <div class="text">
+                        <h2>Lorem ipsum dolor sit amet.</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eius quidem qui sint! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, voluptate.</p>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <div class="text">
+                        <h2>Lorem ipsum dolor sit amet.</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum eius quidem qui sint! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, rerum!</p>
+                    </div>
+    
+                    <div class="image" style="background-image: url('./assets/images/content-3.jpg');">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- credibility -->
+        <section id="credibility">
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iusto minus est qui vero eligendi beatae iure.</p>
+            <div class="container wrapper">
+                <div class="box">
+                    <div class="image">
+                        <img src="./assets/images/headshot.jpg" alt="">
+                    </div>
+                <div class="credibility-text">
+                <p class="text"><strong>"</strong>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio accusamus recusandae quisquam labore.<strong>"</strong></p>
+                    <p class="name">- Angela Sawyer</p>
+                    <p class="small">Fabrication Systems</p>
+                </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                        <img src="./assets/images/headshot-2.jpg" alt="">
+                    </div>
+                    <div class="credibility-text">
+                    <p class="text"><strong>"</strong>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio accusamus recusandae quisquam labore.<strong>"</strong></p>
+                    <p class="name">-Hulkleberry Finn</p>
+                    <p class="small">Yakidi Engineering</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="image">
+                        <img src="./assets/images/headshot-3.jpg" alt="">
+                    </div>
+                    <div class="credibility-text">
+                    <p class="text"><strong>"</strong>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio accusamus recusandae quisquam labore.<strong>"</strong></p>
+                    <p class="name">-Christiane Robin</p>
+                    <p class="small">Taxicon Inc</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- call to action -->
+        <section id="cta">
+            <div id="wrapper">
+                <div id="cta_text">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus?</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque id excepturi voluptatum itaque atque doloribus. Aspernatur porro sunt non.</p>
+                    <button>Sign UP</button>
+                </div>
+                <div id="cta_image">
+                </div>
+            </div>
+        </section>
+    </div>
+    <footer id="footer">
+        <div class="footer-wrapper">
+            <div>
+                <h1>WP Dodge</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quaerat voluptatibus libero eligendi eius!</p>
+            </div>
+            <div class="footer_links">
+                <ul>
+                    <li>Links</li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Proucts & Services</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="footer_links">
+                <ul>
+                    <li>Privacy & Terms</li>
+                    <li><a href="#">Privacy Statement</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">FAQs</a></li>
+                </ul>
+            </div>
+            <div class="newsletter">
+                <h3>Subscribe to our newsletter</h3>
+                <form action="#">
+                    <input type="text" placeholder="Name">
+                    <input type="email" name="" id="" placeholder="Email">
+                    <button>Subscribe</button>
+                </form>
+            </div>
+        </div>
+        <hr>
+        <p id="footer_note">WP Dodge &copy; 2021</p>
+    </footer>
+
+</body>
+</html>
